ce33bda446db5c90813a55d274d07e4dcace1123
ssps12 <38716735+ssps12@users.noreply.github.com>
2019-11-26 16:33:59 +0530

    Update setup_env.sh

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -159,6 +159,7 @@ echo "[Install pandas]"
 sudo chmod -R 777 /home/travis/archiconda3/envs/pandas-dev/lib/python3.7/site-packages
 sudo python3.7 -m pip install numpy
 sudo python3.7 -m pip install --no-build-isolation -e .
+sudo chmod -R 777 $MINICONDA_DIR
 
 echo
 echo "conda list"
