0f87a77042fcb31312b3fa1f28c439076f9c7aba
ssps12 <38716735+ssps12@users.noreply.github.com>
2019-11-26 17:24:46 +0530

    Update run_tests.sh

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -30,7 +30,7 @@ PYTEST_CMD="pytest pandas"
 # Travis does not have have an X server
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     DISPLAY=DISPLAY=:99.0
-    PYTEST_CMD="xvfb-run -e /dev/stdout $PYTEST_CMD"
+    PYTEST_CMD="sudo xvfb-run -e /dev/stdout $PYTEST_CMD"
 fi
 
 echo $PYTEST_CMD
